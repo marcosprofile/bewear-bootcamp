d356aa3078d55442a0956a8a5f1ccf806af81119
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-08-10 14:32:36 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,43 @@
-import Header from "@/components/common/header";
+import Image from "next/image";
 
-export default function Home() {
+import Header from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+
+export default async function Home() {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+      category: true
+    }
+  });
+
   return (
     <div>
       <Header />
-      <h1>Home</h1>
+      <div className="space-y-6">
+        <div className="p-4">
+          <Image
+            src="/banner-mobile.png"
+            alt="Leve uma vida com estilo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+        <ProductList products={products} title="Mais vendidos" />
+        <div className="p-4">
+          <Image
+            src="/banner-2-mobile.png"
+            alt="Autêntico"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/common/product-list.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { productTable, productVariantTable } from "@/db/schema";
+
+import ProductItem from "./product-item";
+
+interface ProductListProps {
+  title: string;
+  products: (typeof productTable.$inferSelect & {
+    variants: (typeof productVariantTable.$inferSelect)[];
+  })[];
+}
+
+export default function ProductList({ title, products }: ProductListProps) {
+  return (
+    <div className="space-y-6">
+      <h3 className="font-semibold px-4">{title}</h3>
+      <div className="flex w-full gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden px-4">
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
