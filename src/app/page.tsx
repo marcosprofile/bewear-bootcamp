f8eece05f86431aeac87dab4d6463b893b6d590c
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-08-10 00:00:07 -0300

    feat: add ui for sign in and sign up form components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import Link from "next/link";
-
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-
-      <Button>Bootcamp</Button>
-      <br />
-      <Link href="/product">Product</Link>
     </div>
   );
 }
