830ebaf8ecffc8850e01bb3f435c548694a7b293
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-09-02 21:54:57 -0300

    feat: add cart summary

--- src/components/common/sheet-cart.tsx
+++ src/components/common/sheet-cart.tsx
@@ -13,7 +13,10 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { formatCentsToBRL } from "@/helpers/money";
 
+import { ScrollArea } from "../ui/scroll-area";
+import { Separator } from "../ui/separator";
 import CartItem from "./cart-item";
 
 export default function SheetCard() {
@@ -39,19 +42,54 @@ export default function SheetCard() {
             </div>
           </SheetTitle>
         </SheetHeader>
-        <div className="space-y-4 px-4">
-          {cartIsLoading && <div>Carregando...</div>}
-          {cart?.items.map((item) => (
-            <CartItem
-              key={item.id}
-              id={item.id}
-              productName={item.productVariant.product.name}
-              productVariantImageUrl={item.productVariant.imageUrl}
-              productVariantName={item.productVariant.name}
-              productVariantPriceInCents={item.productVariant.priceInCents}
-              quantity={item.quantity}
-            />
-          ))}
+
+        <div className="flex h-full flex-col px-5 pb-5">
+          <div className="flex h-full max-h-full flex-col overflow-hidden">
+            <ScrollArea>
+              <div className="flex h-full flex-col gap-8">
+                {cartIsLoading && <div>Carregando...</div>}
+                {cart?.items.map((item) => (
+                  <CartItem
+                    key={item.id}
+                    id={item.id}
+                    productName={item.productVariant.product.name}
+                    productVariantImageUrl={item.productVariant.imageUrl}
+                    productVariantName={item.productVariant.name}
+                    productVariantPriceInCents={
+                      item.productVariant.priceInCents
+                    }
+                    quantity={item.quantity}
+                  />
+                ))}
+              </div>
+            </ScrollArea>
+          </div>
+          {cart?.items.length > 0 && (
+            <div className="flex flex-col gap-4">
+              <Separator />
+
+              <div className="flex items-center justify-between text-xs font-medium">
+                <p>Subtotal</p>
+                <p>{formatCentsToBRL(cart?.totalPriceInCents ?? 0)}</p>
+              </div>
+
+              <Separator />
+
+              <div className="flex items-center justify-between text-xs font-medium">
+                <p>Entrega</p>
+                <p>GRÁTIS</p>
+              </div>
+
+              <Separator />
+
+              <div className="flex items-center justify-between text-xs font-medium">
+                <p>Total</p>
+                <p>{formatCentsToBRL(cart?.totalPriceInCents ?? 0)}</p>
+              </div>
+
+              <Button className="mt-5 rounded-full">Finalizar compra</Button>
+            </div>
+          )}
         </div>
       </SheetContent>
     </Sheet>
